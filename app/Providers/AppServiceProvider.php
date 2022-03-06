ea151725eb1cb3f0969a86733ae70eed7c577a66
petermalak <pmalak14@gmail.com>
2022-03-06 16:43:36 +0200

    test https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+        Blade::if("dev", function () {
+            return app()->env == "local" || app()->env == "dev" || app()->env == "development";
+        });
+
+        // if (App::environment('production') ||  env("APP_ENV", "production"))
+        // {
+        //    resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+        //    $this->app['request']->server->set('HTTPS', true);
+
+        // }
     }
 }
