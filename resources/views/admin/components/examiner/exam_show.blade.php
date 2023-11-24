0184e78af0848187a4b78b47df0a865bbe529570
Yoyussef Salah <youssefsalahcs@gmail.com>
2023-11-24 11:51:54 +0200

    show answer

--- resources/views/admin/components/examiner/exam_show.blade.php
+++ resources/views/admin/components/examiner/exam_show.blade.php
@@ -16,6 +16,29 @@
         </div>
     </section>
 
+        <div class="container mt-5">
+        <div class="card">
+            <div class="card-header">
+                <h1 class="text-center">تعليمات هامة</h1>
+            </div>
+            <div class="card-body text-center">
+                <?php
+                if ($instructionsObject !== null) {
+                    $instructionsLines = explode("+", $instructionsObject);
+                    foreach ($instructionsLines as $line) {
+                        echo "<p>{$line}+</p>";
+                    }
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+    <div class="card-body text-center">
+        <h2><span>اسم الفصل : {{$class_name}}</span></h2><span><h2>اسم خادم الفصل : {{$teacher_name}}</h2></span><span><h2> رقم تليفون الخادم :  {{$teacher_phone}}</h2></span>
+    </div>
+
+
+
     <!-- Main content -->
     <section class="content">
 
@@ -80,6 +103,7 @@
                                     <th>السؤال</th>
                                     <th>درجة السؤال</th>
                                     <th>الدرجة</th>
+                                    <th>إظهار الإجابة</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -89,10 +113,11 @@
                                     @endphp --}}
                                     <tr>
                                         {{-- <td id="question_number">{{ $key }}</td> --}}
-                                        <td id="question">{{ $item->question }}</td>
-                                        <input type="hidden" class="form-control input-name"
-                                            id="question-{{ $key }}"
-                                            name="answers[{{ $key }}][question]" value="{{ $item->question }}">
+                                        <td id="question">
+        <input type="hidden" class="form-control input-name original-question"
+            name="answers[{{ $key }}][original-question]" value="{{ $item->question }}">
+        {{ $item->question }}
+    </td>
                                         <td id="wight">{{ $item->wight }}</td>
                                         <input type="hidden" class="form-control input-name" id="wight{{ $key }}"
                                             name="answers[{{ $key }}][wight]" value="{{ $item->wight }}">
@@ -101,6 +126,11 @@
                                                 class="form-control input-name" id="mark-{{ $key }}"
                                                 name="answers[{{ $key }}][score]" required
                                                 value="{{ $item->score }}">
+                                        </td>
+                                        <td>
+                                            <button type="button" class="btn btn-info show-answer-btn" data-answer="{{ $answers[$key] }}">
+                                            Show Answer
+                                            </button>
                                         </td>
                                     </tr>
                                 @endforeach
@@ -112,4 +142,30 @@
             </div>
         </div>
     </section>
+    <script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const showAnswerButtons = document.querySelectorAll('.show-answer-btn');
+
+        showAnswerButtons.forEach(function (button) {
+            // Store the original question outside the loop
+            const originalQuestion = button.closest('tr').querySelector('.original-question').value;
+
+            button.addEventListener('click', function () {
+                const row = this.closest('tr');
+                const answerCell = row.querySelector('#question');
+                const answer = this.getAttribute('data-answer');
+
+                if (this.textContent.trim() === 'Show Answer') {
+                    answerCell.innerHTML = answer; // Show the answer
+                    this.textContent = 'Hide Answer';
+                } else {
+                    answerCell.innerHTML = originalQuestion; // Show the original question
+                    this.textContent = 'Show Answer';
+                }
+            });
+        });
+    });
+</script>
+
+
 @endsection
